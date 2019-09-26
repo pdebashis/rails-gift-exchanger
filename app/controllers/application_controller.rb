277b6b768041906724d756aa798c881dc5eefa12
pdebashis <pdebashis@outlook.com>
2019-09-27 01:43:44 +0530

    Static pages and Initial mock-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def home
+  	render 'static pages/home'
+  end
+
+  def contact
+    render "static pages/contact"
+  end
+
+  def terms_of_use
+    render "static pages/terms_of_use"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :exchanges
+  root 'application#home'
+
+  get '/contact', to: 'application#contact'
+  get "/terms_of_use", to: "application#terms_of_use"
 end
