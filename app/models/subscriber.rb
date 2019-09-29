0769611c98f2e611a655134d2a4f707cd384810a
pdebashis <pdebashis@outlook.com>
2019-09-30 01:06:19 +0530

    Issue Fixed for email trailing spaces

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,8 @@
 class Subscriber < ApplicationRecord
+  before_validation :strip_whitespace
   validates :sub_email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Please enter a valid email address"}
+
+  def strip_whitespace
+    self.sub_email = self.sub_email.strip unless self.sub_email.nil?
+  end
 end
