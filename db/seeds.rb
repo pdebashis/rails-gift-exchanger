79962f3be336738d8838efd17ef5c81ce8ceeb5d
pdebashis <pdebashis@outlook.com>
2019-10-18 16:47:16 +0530

    seeded exchanges and restrict access

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,7 @@ Subscriber.delete_all
 Subscriber.create!([{sub_email: "dabbebhutdp@gmail.com",type_of_sub: "FrontPage Subscriber",subscribed: true},
 	{sub_email: "sirishapalivela95@gmail.com",type_of_sub: "FrontPage Subscriber",subscribed: true}
 	])
+
+Exchange.delete_all
+
+Exchange.create!([{title: "Christmas 2019",date_of_gifting: "2019-12-21",finish_date: "2019-12-26", date_of_matching: "2019-12-12"}])
