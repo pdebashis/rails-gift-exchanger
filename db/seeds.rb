a36e741c8ebee3b0bd4813c6d9df6237d3e2090f
pdebashis <pdebashis@outlook.com>
2019-12-11 23:51:18 +0530

    seed file added 4th member

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.create!([{email: "dabbebhutdp@gmail.com",name: "Debashis", is_admin: true, 
 
 
 Exchange.create!([{title: "Christmas 2019",date_of_gifting: "2019-12-21",finish_date: "2019-12-26", date_of_matching: "2019-12-12", user_id: User.first.id}])
-Exchange.create!([{title: "Christmas at XYZ",date_of_gifting: "2019-12-21",finish_date: "2019-12-26", date_of_matching: "2019-12-12", user_id: User.first.id}])
+Exchange.create!([{title: "Test Exchange",date_of_gifting: "2019-12-21",finish_date: "2019-12-26", date_of_matching: "2019-12-12", user_id: User.first.id}])
 
 exchange1 = Exchange.first
 exchange2 = Exchange.last
@@ -26,11 +26,14 @@ exchange2 = Exchange.last
 member1 = exchange1.members.build({email: "dabbebhutdp@gmail.com"})
 member2 = exchange2.members.build({email: "sirishapalivela95@gmail.com"})
 member3 = exchange2.members.build({email: "test_user1@gmail.com"})
+member4 = exchange2.members.build({email: "dabbebhutdp@gmail.com"})
 
 member1.user = User.find_by({email: "dabbebhutdp@gmail.com"})
+member4.user = User.find_by({email: "dabbebhutdp@gmail.com"})
 member2.user = User.find_by({email: "sirishapalivela95@gmail.com"})
 member3.user = User.find_by({email: "test_user1@gmail.com"})
 
 member1.save
 member2.save
 member3.save
+member4.save
