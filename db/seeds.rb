2c0a4028d5dfddb7b5f63e52a9cae9c5ac53eb40
pdebashis <pdebashis@outlook.com>
2019-12-16 00:52:55 +0530

    updated seeds on new db schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,29 +11,15 @@ User.delete_all
 Subscriber.delete_all
 
 
-User.create!([{email: "dabbebhutdp@gmail.com",name: "Debashis", is_admin: true, password: "password"},
-	{email: "sirishapalivela95@gmail.com",is_admin: false,name: "Sirisha", password: "password"},
-    {email: "test_user1@gmail.com",is_admin: false, password: "password"}])
+User.create!([{email: "dabbebhutdp@gmail.com",name: "Debashis", is_admin: true, password: "password"}])
 
 
-Exchange.create!([{title: "Christmas 2019",date_of_gifting: "2019-12-21",finish_date: "2019-12-26", date_of_matching: "2019-12-12", user_id: User.first.id}])
-Exchange.create!([{title: "Test Exchange",date_of_gifting: "2019-12-21",finish_date: "2019-12-26", date_of_matching: "2019-12-12", user_id: User.first.id}])
+Exchange.create!([{title: "Christmas 2019",date_of_gifting: "2019-12-21", date_of_matching: "2019-12-12", user_id: User.first.id}])
+Exchange.create!([{title: "Test Exchange",date_of_gifting: "2019-12-21", date_of_matching: "2019-12-12", user_id: User.first.id}])
 
 exchange1 = Exchange.first
-exchange2 = Exchange.last
 
 
 member1 = exchange1.members.build({email: "dabbebhutdp@gmail.com"})
-member2 = exchange2.members.build({email: "sirishapalivela95@gmail.com"})
-member3 = exchange2.members.build({email: "test_user1@gmail.com"})
-member4 = exchange2.members.build({email: "dabbebhutdp@gmail.com"})
-
 member1.user = User.find_by({email: "dabbebhutdp@gmail.com"})
-member4.user = User.find_by({email: "dabbebhutdp@gmail.com"})
-member2.user = User.find_by({email: "sirishapalivela95@gmail.com"})
-member3.user = User.find_by({email: "test_user1@gmail.com"})
-
 member1.save
-member2.save
-member3.save
-member4.save
